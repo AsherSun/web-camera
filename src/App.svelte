0df3539ddf49a6dfdff2ec61ae246e9837e43472
无始 <wushi@yinge.tech>
2021-11-23 17:23:27 +0800

    docs(readme): 修改文档

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,9 @@
 
 <main>
 	<button on:click={drawImage}>拍照</button>
-	<!-- svelte-ignore a11y-media-has-caption -->
-	<video  id="video" />
-	<canvas id="canvas" width="500" height="500" />
 	{#if imageData}
 	<img src={imageData} alt="" id="image">
 	{/if}
-	
-	<!-- <button on:click={getUserMedia}>打开媒体设备</button> -->
-	<!-- <button on:click={drawImage}>拍照</button> -->
 </main>
 
 
@@ -19,7 +13,6 @@ import { onMount } from "svelte";
 
 
 let video: HTMLVideoElement;
-let canvas: HTMLCanvasElement;
 let imageData: string;
 const width = 500;
 const height = 500;
@@ -30,12 +23,14 @@ onMount(() => {
 
 function getUserMedia() {
 	navigator.mediaDevices.getUserMedia({video: {width: width, height: height}}).then((stream) => {
-		video = document.querySelector('#video') as HTMLVideoElement;
+		video = document.createElement('video');
+		video.width = width;
+		video.height = height;
 		video.src = stream as never as string;
 		video.srcObject = stream;
 		video.play();
-	}).catch((error) => {
-		console.log('error', JSON.stringify(error))
+	}).catch((error: Error) => {
+		console.log('error', error.message, error.stack, error.name)
 		console.log('访问用户媒体设备失败，请确保使用的是Edge或者Chrome浏览器！');
 		console.log('请确保摄像头可访问')
 		console.log('Edge浏览器中的设置: edge://settings/content/camera');
@@ -48,7 +43,9 @@ function drawImage() {
 		alert('请先打开摄像头')
 		return;
 	}
-	canvas = document.querySelector('#canvas') as HTMLCanvasElement;
+	const canvas = document.createElement('canvas');
+	canvas.width = width;
+	canvas.height = width;
 	const context = canvas.getContext('2d');
 	context.drawImage(video, 0, 0, width, height, 0, 0, width, height);
 	imageData = canvas.toDataURL('image/png');
@@ -67,10 +64,6 @@ function drawImage() {
 		main {
 			max-width: none;
 		}
-	}
-	#video {
-		width: 500px;
-		height: 500px;
 	}
   #image {
 		width: 500px;
